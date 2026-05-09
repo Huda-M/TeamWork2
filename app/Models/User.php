fee6f6eb57f9fe94a5d6fc765a41a0db7584bf12
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-09 04:03:44 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens , SoftDeletes;;
 
     protected $fillable = [
         'full_name',
