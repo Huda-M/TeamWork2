398059828394e908aa39c329d6ddfd20510dacd9
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-14 04:17:09 +0300

    Update OpenApi.php

--- app/Swagger/OpenApi.php
+++ app/Swagger/OpenApi.php
@@ -4,11 +4,26 @@ namespace App\Swagger;
 
 use OpenApi\Annotations as OA;
 
-
 /**
+ * @OA\Info(
+ *     title="Team Work API",
+ *     version="1.0.0",
+ *     description="API Documentation"
+ * )
+ *
  * @OA\Server(
  *     url=L5_SWAGGER_CONST_HOST,
  *     description="Production Server"
  * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="Bearer",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="Enter your Bearer token"
+ * )
  */
-class OpenApi {}
+class OpenApi
+{
+}
