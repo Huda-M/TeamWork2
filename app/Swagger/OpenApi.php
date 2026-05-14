f5c56b6abda0ad22729809ff54f7ca6fc6483183
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-14 03:17:50 +0300

    Update OpenApi.php

--- app/Swagger/OpenApi.php
+++ app/Swagger/OpenApi.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="Team Work API",
+ *     version="1.0.0",
+ *     description="API Documentation"
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Local Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="Bearer",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="Enter your Bearer token"
+ * )
+ */
+class OpenApi
+{
+}
