7d5b4aa7f1ef85ea5b97feaea6caa0df033f3b1a
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-14 06:00:09 +0300

    Update OpenApi.php

--- app/Swagger/OpenApi.php
+++ app/Swagger/OpenApi.php
@@ -13,8 +13,20 @@ use OpenApi\Annotations as OA;
  * )
  * @OA\Server(
  *     url=L5_SWAGGER_CONST_HOST,
- *     description="Production Server"
+ *     description="Server"
  * )
+ * @OA\SecurityScheme(
+ *     securityScheme="Bearer",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT",
+ *     description="Enter your Bearer token"
+ * )
+ * @OA\Tag(name="Tasks", description="Task Management")
+ * @OA\Tag(name="Teams", description="Team Management")
+ * @OA\Tag(name="Projects", description="Project Management")
+ * @OA\Tag(name="Statistics", description="Statistics")
+ * @OA\Tag(name="Reports", description="Reports")
  */
 
 class OpenApi
