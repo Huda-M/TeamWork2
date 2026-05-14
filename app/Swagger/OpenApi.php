ec06b7e88f2aaab6fc2f747fd4a175ca0c28b0e3
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-14 05:23:20 +0300

    Update OpenApi.php

--- app/Swagger/OpenApi.php
+++ app/Swagger/OpenApi.php
@@ -6,6 +6,11 @@ use OpenApi\Annotations as OA;
 
 
 /**
+ * @OA\Info(
+ *     title="Team Work API",
+ *     version="1.0.0",
+ *     description="API Documentation"
+ * )
  * @OA\Server(
  *     url=L5_SWAGGER_CONST_HOST,
  *     description="Production Server"
