fb6245f1052e575620673e62469c5308b79071a3
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-15 18:44:01 +0300

    Create OpenApi.php

---
+++ app/Http/Controllers/OpenApi.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="Team Work API",
+ *     version="1.0.0"
+ * )
+ */
+class OpenApi
+{
+}
