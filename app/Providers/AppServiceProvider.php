14560f9fcab9088f0e2597bf5736623a503642c6
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-15 19:36:05 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use OpenApi\Analysers\AnnotationAnalyser;
+use L5Swagger\Generator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Custom password reset URL
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        // Set Swagger annotation analyser only in local environment
+        if ($this->app->environment('local')) {
+            $this->app->extend(Generator::class, function ($generator, $app) {
+                $generator->setAnalyser(new AnnotationAnalyser());
+                return $generator;
+            });
+        }
     }
 }
