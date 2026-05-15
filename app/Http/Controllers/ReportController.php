e8e4adcc11b222b8528ae63ed76492039d20b615
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-15 16:28:46 +0300

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
+use OpenApi\Annotations as OA;
 
 class ReportController extends Controller
 {
