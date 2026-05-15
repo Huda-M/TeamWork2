8abb1bd89b9d360d8e063272808911515e7c6a39
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-15 20:06:26 +0300

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,9 +15,27 @@ use Illuminate\Support\Facades\Log;
 use OpenApi\Annotations as OA;
 
 /**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Team Work API",
+ *     description="API Documentation"
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Server"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     securityScheme="Bearer",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ *
  * @OA\Tag(
  *     name="Tasks",
- *     description="المهام والتو-دو"
+ *     description="Task management"
  * )
  */
 class TaskController extends Controller
