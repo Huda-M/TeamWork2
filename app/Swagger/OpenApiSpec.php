fc16406d6779bec915fabacbd2347caa643ca879
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-19 00:16:28 +0300

    Create OpenApiSpec.php

---
+++ app/Swagger/OpenApiSpec.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="This is my Laravel API",
+ *     @OA\Contact(
+ *         email="admin@example.com"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
+ * )
+ */
+class OpenApiSpec
+{
+    // هذا الكلاس قد يكون فارغًا، المهم وجود التعليقات أعلاه
+}
