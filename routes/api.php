1c4d8d6e29e07fb515c0c9917d04ee18b763bf86
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-19 18:46:46 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -184,7 +184,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::prefix('profile')->group(function () {
     Route::get('/', [ProfileController::class, 'myProfile']);           // ✅ صحيح
-    Route::put('/update', [ProfileController::class, 'updateProfile']);
+    Route::post('/update', [ProfileController::class, 'updateProfile']);
     Route::get('/my-stats', [ProfileController::class, 'myStats']);
     Route::get('/my-evaluations', [ProfileController::class, 'myEvaluations']);
     Route::get('/team-members/{projectId}/to-evaluate', [ProfileController::class, 'teamMembersToEvaluate']);
