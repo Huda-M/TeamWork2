98e16dc05d6f87cb671fbdcb8099f763f60f4553
Huda-M <hodaelawady@std.mans.edu.eg>
2026-05-22 20:22:06 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -101,6 +101,7 @@ Route::middleware('start.session')->group(function () {
 
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/teams/{teamId}/my-ratings', [TeamController::class, 'getTeamMembersWithMyRatings']);
     Route::get('/teams/{id}/basic-details', [TeamController::class, 'getTeamBasicDetails']);
     Route::get('/my/level-progression', [ProgrammerController::class, 'levelProgression']);
     Route::get('/my/dashboard', [ProgrammerController::class, 'dashboard']);
