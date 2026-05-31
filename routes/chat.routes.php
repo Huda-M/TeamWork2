c23b8f7eb8d7aba57123d411e81df122b1f9f41f
SehamMohsen <sehammohsen84@gmail.com>
2026-06-01 02:06:59 +0300

    feat: register team chat routes for message retrieval and sending

--- routes/chat.routes.php
+++ routes/chat.routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TeamChatController;
+use Illuminate\Support\Facades\Route;
 
 Route::controller(TeamChatController::class)->prefix('teams')->middleware('auth:sanctum')->group(function () {
     Route::get('/{team}/chat/messages', 'messages');
