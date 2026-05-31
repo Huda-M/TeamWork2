fd620f46ad1282c2911038fb50bc98cd5617275c
SehamMohsen <sehammohsen84@gmail.com>
2026-05-31 05:02:16 +0300

    feat: create messages table migration with chat room and user associations

--- database/migrations/2026_05_30_224122_create_messages_table.php
+++ database/migrations/2026_05_30_224122_create_messages_table.php
@@ -11,13 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('messages', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('chat_room_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->text('body');
-            $table->timestamps();
-        });
+
+        if (! Schema::hasTable('messages')) {
+            Schema::create('messages', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('chat_room_id')->constrained()->cascadeOnDelete();
+                $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+                $table->text('body');
+                $table->timestamps();
+            });
+        }
+
     }
 
     /**
