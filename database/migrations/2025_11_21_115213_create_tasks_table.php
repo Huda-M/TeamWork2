d0396e86c8fd90f4c7b409cd16dbc0e219c4c969
Huda-M <hodaelawady@std.mans.edu.eg>
2026-06-17 16:09:46 +0300

    Update 2025_11_21_115213_create_tasks_table.php

--- database/migrations/2025_11_21_115213_create_tasks_table.php
+++ database/migrations/2025_11_21_115213_create_tasks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('programmer_id')->constrained();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->enum('status',['todo', 'in_progress', 'review', 'done']);
+            $table->enum('status',['todo','active','done']);
             $table->integer('estimated_hours');
             $table->integer('actual_hours')->nullable();
             $table->timestamp('deadline');
