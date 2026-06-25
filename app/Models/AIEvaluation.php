cf897e8175ebe9dff909a2f5dc5225f52ead6a95
Huda-M <hodaelawady@std.mans.edu.eg>
2026-06-25 15:17:51 +0300

    Create AIEvaluation.php

---
+++ app/Models/AIEvaluation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AIEvaluation extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'team_id',
+        'evaluator_id',
+        'evaluated_id',
+        'overall_score',
+        'breakdown',
+        'explanation',
+        'is_ai_generated',
+    ];
+
+    protected $casts = [
+        'breakdown' => 'array',
+        'overall_score' => 'decimal:2',
+        'is_ai_generated' => 'boolean',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function evaluator()
+    {
+        return $this->belongsTo(Programmer::class, 'evaluator_id');
+    }
+
+    public function evaluated()
+    {
+        return $this->belongsTo(Programmer::class, 'evaluated_id');
+    }
+}
