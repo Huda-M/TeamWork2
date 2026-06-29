49985f66676603768291534ef0f068f5305f58bb
SehamMohsen <sehammohsen84@gmail.com>
2026-06-29 22:43:25 +0300

    feat: add authentication and profile management routes for company entities

--- routes/Companies/auth.routes.php
+++ routes/Companies/auth.routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\SocialAuthController;
 use App\Http\Controllers\Company\Auth\AuthController;
 use App\Http\Controllers\Company\Auth\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -9,8 +10,8 @@ Route::controller(AuthController::class)->prefix('company/auth')->group(function
     Route::post('/register', 'register');
 });
 
-Route::get('/auth/{provider}/redirect', [AuthController::class, 'redirectToProvider']);
-Route::get('/auth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
+Route::get('/company/auth/{provider}/redirect', [SocialAuthController::class, 'redirectToProvider']);
+Route::get('/company/auth/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']);
 
 Route::controller(AuthController::class)->prefix('company/auth')->middleware('auth:sanctum')->group(function () {
     Route::post('/logout', 'logout');
