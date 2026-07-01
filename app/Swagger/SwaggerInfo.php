67e9872f37ee0984dde4e9f29d4d54a8adda6a3d
Huda-M <hodaelawady@std.mans.edu.eg>
2026-07-01 17:00:05 +0300

    Add SwaggerInfo with @OA\Info annotation to enable l5-swagger generation

---
+++ app/Swagger/SwaggerInfo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *   version="1.0.0",
+ *   title="TeamWork2 API",
+ *   description="Documentation for TeamWork2"
+ * )
+ */
+class SwaggerInfo
+{
+    // This class only holds the OpenAPI docblock.
+}
