ce5f5d8147112af164f19fb8843272f4eeb61ac5
Huda-M <hodaelawady@std.mans.edu.eg>
2026-07-01 16:31:35 +0300

    Add OpenAPI annotations to Controller.php
    Added OpenAPI annotations for API documentation and security scheme.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Bridge X API",
+ *     description="API documentation for Bridge X platform",
+ *     @OA\Contact(
+ *         email="support@bridgex.com"
+ *     )
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="Bearer",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ */
 abstract class Controller
 {
     //
